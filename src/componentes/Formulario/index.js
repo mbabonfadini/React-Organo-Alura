0b19692f997b5ff5ef174917e4aec4cefad89bd4
mbabonfadini <mbabonfadini@gmail.com>
2023-01-30 22:09:40 -0300

    Obtendo dados do formulario com o React

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,8 +1,10 @@
 import './Formulario.css'
 import CampoTexto from '../CampoTexto'
 import ListaSupensa from '../ListaSuspensa'
+import Botao from '../Botao/Index'
+import { useState } from 'react'
 
-const Formulario = () => {
+const Formulario = (props) => {
     const times = [
         'Programação',
         'Front-end',
@@ -11,15 +13,49 @@ const Formulario = () => {
         'Mobile',
         'Inovação e Gestão'
     ]
+    const [nome,setNome] = useState('')
+    const [cargo,setCargo] = useState('')
+    const [imagem,setImagem] = useState('')
+    const [time,setTime] = useState('')
+
+    const aoSalvar = (evento)=>{
+        evento.preventDefault()
+        props.aoColaboradorCadastrado(
+            {nome,cargo,imagem,time})
+    }
 
     return (
         <section className="formulario">
-            <form className="">
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
-                <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
-                <ListaSupensa label="Time" itens={times}/>
+                <CampoTexto 
+                obrigatorio={true} 
+                label="Nome" 
+                placeholder="Digite seu nome"
+                valor={nome}
+                aoAlterado={valor=> setNome(valor)}/>
+
+                <CampoTexto 
+                obrigatorio={true} 
+                label="Cargo" 
+                placeholder="Digite seu cargo"
+                valor={cargo}
+                aoAlterado={valor=>setCargo(valor)}/>
+
+                <CampoTexto 
+                label="Imagem" 
+                placeholder="Informe o endereço da imagem"
+                valor={imagem}
+                aoAlterado={valor=>setImagem(valor)}/>
+
+                <ListaSupensa 
+                obrigatorio={true} 
+                label="Time" 
+                itens={times}
+                valor={time}
+                aoAlterado={valor=>setTime(valor)}/>
+
+                <Botao texto="Criar card">Cria Card</Botao>
             </form>
         </section>
     )
--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -1,12 +1,18 @@
 import "./ListaSupensa.css"
 
 const ListaSupensa = (props)=>{
+    
+    const aoTrocar = (evento)=>{
+        props.aoAlterado(evento.target.value)
+    }
+    
     return (
         <div className="lista-suspensa">
             <label>
                 {props.label}
             </label>
-            <select>
+            <select required={props.obrigatorio} value={props.valor} onChange={aoTrocar}>
+                <option></option>
                 {props.itens.map(item=> <option key={item}>{item}</option>)}
             </select>
             
